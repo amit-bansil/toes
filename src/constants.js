98e1ef123bd2b6505378a21b98c8371be0b647e0
Amit Bansil <amit@bansil.org>
2017-01-19 19:42:00 -0500

    docs: clarify constants

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,16 @@
 export const MIN_BOARD_SIZE = 4;
 export const MAX_BOARD_SIZE = 7;
-export const WINNING_SCORE = 5;
-export const INITIAL_DIMENSIONS = [4, 4];
-export const DEFAULT_PLAYER_USER = "Human";
 export const MIN_WIN_LENGTH = 4;
 export const MAX_WIN_LENGTH = 7;
+
+export const INITIAL_DIMENSIONS = [4, 4];
 export const INITIAL_WIN_LENGTH = 4;
+
+// number of rounds a player must win to win game
+export const WINNING_SCORE = 5;
+
+export const DEFAULT_PLAYER_USER = "Human";
+
 export const PLAYER_COLORS = [
   "#56d179",
   "#56aed1",
